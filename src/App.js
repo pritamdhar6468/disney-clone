779c83e52c1f82788e5bd76082e8f8814378751e
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-24 11:12:19 +0530

    container modification

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/">
+          <Route exact path="/" >
             <Login />
           </Route>
         </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,7 @@
-import styled from "styled-components";
+
 
 const Login= (props)=>{
-    return <div>LOGIN</div>
+    return <div>LOGIN</div>;
 };
-const Container=styled.section`
-overflow:hidden;
-display;
-`;
+
 export default Login;
